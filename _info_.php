f153070e904385bf0812968c1d33f16b88701808
xtr4nge <xtr4nge@gmail.com>
2013-11-08 10:44:58 +0000

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="mdk3";
-$mod_version="1.0";
+$mod_version="1.1";
 $mod_logs="/usr/share/FruityWifi/logs/mdk3.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/mdk3/includes/logs/";
 $mod_path="/usr/share/FruityWifi/www/modules/mdk3";
